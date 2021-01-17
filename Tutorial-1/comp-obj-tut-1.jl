9e92f34e0ed03be5baa1e23e946557b3ad03716a
Arif Shaikh <arif.shaikh@icts.res.in>
2021-01-17 23:13:26 +0530

    plots added

--- Tutorial-1/comp-obj-tut-1.jl
+++ Tutorial-1/comp-obj-tut-1.jl
@@ -11,7 +11,6 @@ function stellar_structure!(du, u, params, m)
     du[2] = (t^b / (x^2 * p^a))
     du[3] = - (p^(a * n) * l) / (x^4 * t^(3 + s + b * n))
     du[4] = A * p^(a * lambda) * t^(nu - (b * lambda))
-    # @printf("m = %.f \t dp/dm = %.f dx/dm = %.f \t dt/dm = %.f \t dl/dm = %.f \t p = %.f\n", m, du[1], du[2], du[3], du[4], p)
 end
 
 # We define the condition to find the event where p becomes less than some tolerace set by user.
@@ -21,7 +20,7 @@ cb = ContinuousCallback(condition, affect!)
 
 # Initial conditions, at m = 0, as per eq 1.2
 p0 = 1.0
-x0 = 1e-3
+x0 = 1e-4
 t0 = 1.0
 l0 = 0.0
 
@@ -34,11 +33,16 @@ n = 0.0
 s = 0.0
 nu = 4.0
 lambda = 1.0
-A = 0.5
+A = 0.535
 
 params = [a, b, n, s, nu, lambda, A]
-mspan = (1e-9, 20.0)
-prob1 = ODEProblem(stellar_structure!, u0, mspan, params)
-sol1 = solve(prob1, alg_hints=[:stiff], callback=cb)
-plot(sol1, vars=(0, 1), lw = 2, xaxis = (L"m"), yaxis = (L"p(m)"))
-savefig("p_vs_m.pdf")
+mspan = (0, 20.0)
+problem1 = ODEProblem(stellar_structure!, u0, mspan, params)
+solution1 = solve(problem1, alg_hints=[:stiff], callback=cb)
+println(solution1.t[end], "\t", solution1.u[end][1])
+plot(solution1, vars=(0, 1), lw = 2, xlabel = L"m", label = L"p")
+plot!(solution1, vars=(0, 2), lw = 2, xlabel = L"m", label = L"x")
+plot!(solution1, vars=(0, 3), lw = 2, xlabel = L"m", label = L"t")
+plot!(solution1, vars=(0, 4), lw = 2, xlabel = L"m", label = L"l")
+plot!(title = L"A=%$A", titlefontsize = 10, xlims = [0, 12], ylims = [0, 5])
+savefig("p_vs_m_A_$A.pdf")
